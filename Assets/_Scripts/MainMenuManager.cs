df58f1012ac39cbee11329340fab3cb8d2eb1402
Alexander Evstratov <alexdrugev@me.com>
2024-05-24 15:22:29 +0300

    Add best score to menu scene.

--- Assets/_Scripts/MainMenuManager.cs
+++ Assets/_Scripts/MainMenuManager.cs
@@ -7,12 +7,14 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _coinsText; 
+    [SerializeField] private TMP_Text _bestScoreText;
     public static int Coins;
 
     private void Start()
     {
         Time.timeScale = 1;
         Coins = PlayerPrefs.GetInt("coinsCount", 0);
+        _bestScoreText.text = "BEST: " + PlayerPrefs.GetInt("bestScore", 0).ToString();
     }
 
     public void StartGame()
